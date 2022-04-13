57f6a88bf7142af5681da792c369b752ae0d7bdf
maximthomas <maxim.thomas@gmail.com>
2022-04-13 16:12:40 +0300

    update spring & add logging

--- sample-service/src/main/java/org/openidentityplatform/sampleservice/SampleServiceApplication.java
+++ sample-service/src/main/java/org/openidentityplatform/sampleservice/SampleServiceApplication.java
@@ -4,11 +4,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -26,9 +29,9 @@ public class SampleServiceApplication {
     }
 
     @RestController
-    public class IndexController {
+    public static class IndexController {
         @RequestMapping("/")
-        public Map<String, String> index() {
+        public Map<String, String> index(@RequestBody String body) {
             return Collections.singletonMap("hello", "world");
         }
 
@@ -54,12 +57,12 @@ public class SampleServiceApplication {
             
             Map<String, Object> jwtMap = new HashMap<>();
             
-            String аuthorizationList=((HttpServletRequest) request).getHeader("Authorization");
-    		if (StringUtils.isNotBlank(аuthorizationList)){
-    			for (String аuthorization: new HashSet<String>(Arrays.asList(аuthorizationList.split(",|;")))){
-    				аuthorization=StringUtils.stripToEmpty(аuthorization);
-    				if (StringUtils.startsWithIgnoreCase(аuthorization, "Bearer")) {
-    					String jwt = аuthorization.replaceFirst("Bearer\\s+", "");
+            String authList= request.getHeader("Authorization");
+    		if (StringUtils.isNotBlank(authList)){
+    			for (String authorization: new HashSet<>(Arrays.asList(authList.split("[,;]")))){
+    				authorization=StringUtils.stripToEmpty(authorization);
+    				if (StringUtils.startsWithIgnoreCase(authorization, "Bearer")) {
+    					String jwt = authorization.replaceFirst("Bearer\\s+", "");
     					
     					System.out.println("------------ Decode JWT ------------");
 			            String[] split_string = jwt.split("\\.");
@@ -86,4 +89,16 @@ public class SampleServiceApplication {
             return result;
         }
     }
+
+	@Bean
+	public CommonsRequestLoggingFilter logFilter() {
+		CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+
+		filter.setIncludeQueryString(true);
+		filter.setIncludePayload(true);
+		filter.setMaxPayloadLength(10000);
+		filter.setIncludeHeaders(true);
+
+		return filter;
+	}
 }
